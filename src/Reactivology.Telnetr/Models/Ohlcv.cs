099ad7346af7c2b3357be57b361932af652463bb
Michael Park <migrap@gmail.com>
2014-06-28 15:48:41 -0700

    Added Vwap

--- src/Reactivology.Telnetr/Models/Ohlcv.cs
+++ src/Reactivology.Telnetr/Models/Ohlcv.cs
@@ -20,8 +20,10 @@ namespace Reactivology.Telnetr.Models {
 
         public long Count { get; set; }
 
+        public double Vwap { get; set; }
+
         public override string ToString() {
-            return string.Format("[Ohlcv: Datetime={0}, Timespan={1}, Symbol={2}, Open={3}, High={4}, Low={5}, Close={6}, Volume={7}, Count={8}]", Datetime, Timespan, Symbol, Open, High, Low, Close, Volume, Count);
+            return string.Format("[Ohlcv: Datetime={0}, Timespan={1}, Symbol={2}, Open={3}, High={4}, Low={5}, Close={6}, Volume={7}, Count={8}, Vwap={9}]", Datetime, Timespan, Symbol, Open, High, Low, Close, Volume, Count, Vwap);
         }
     }    
 }
